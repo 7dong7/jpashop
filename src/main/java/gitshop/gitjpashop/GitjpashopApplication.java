db2187dd482f509d1a37c9d34cdf4409be5daa7a
7dong7 <tmdkdl777@gmail.com>
2024-08-24 21:33:22 +0900

    1. create git shop

---
+++ src/main/java/gitshop/gitjpashop/GitjpashopApplication.java
@@ -0,0 +1,14 @@
+package gitshop.gitjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitjpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitjpashopApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/gitshop/gitjpashop/GitjpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package gitshop.gitjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitjpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
